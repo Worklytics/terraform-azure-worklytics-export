fc06fb6bb3b32185d14674749ef8394864eafb3b
davidfq <davidfq@gmail.com>
2024-03-26 19:07:56 +0100

    Add output for CI action

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "worklytics_export_container" {
   description = "The Terraform resource created as the export container. See https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_container for details."
 }
 
+output "worklytics_export_app_client_id" {
+  value       = azuread_application.worklytics.client_id
+  description = "The Entra App for federated access to the container"
+}
+
 output "todo_markdown" {
   value       = var.todos_as_outputs ? local.todo_content : null
   description = "Actions that must be performed outside of Terraform (markdown format)."
